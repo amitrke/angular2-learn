615072263ce20dd64ad7e2a4a658dbd5579e2a82
amitrke <amitrke@gmail.com>
2017-08-04 00:13:12 -0400

    Webservice call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  data: any = {};
+  constructor(private http:Http){
+    this.getValues();
+    this.getData();
+  }
+
+  getData(){
+    return this.http.get('https://08fqotbbsa.execute-api.us-east-1.amazonaws.com/beta')
+    .map((res:Response) => res.json());
+  }
+
+  getValues(){
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    })
+  }
 }
